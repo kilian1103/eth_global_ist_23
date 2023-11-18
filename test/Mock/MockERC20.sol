1d2c1d12f1d48c4d442b6d455db716fd3e874a76
kisso-selvan <kisso_selvan@icloud.com>
2023-11-19 02:13:25 +0300

    Troubleshooting

--- test/Mock/MockERC20.sol
+++ test/Mock/MockERC20.sol
@@ -6,7 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockERC20 is ERC20 {
     constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
-    {}
+    {
+        // Mint 1,000,000 tokens to the address deploying the contract
+        _mint(address(this), 1000000 * 10 ** uint(decimals()));
+    }
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
